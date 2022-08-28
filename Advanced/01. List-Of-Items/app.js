562903a3669a93e3f369c4d9071a2b790752f9ff
Georgi Fotev <georgifotev12@gmail.com>
2022-08-28 19:39:07 +0300

    Adding tasks

---
+++ Advanced/01. List-Of-Items/app.js
@@ -0,0 +1,7 @@
+function addItem() {
+  let text = document.getElementById("newItemText");
+  let li = document.createElement("li");
+  li.textContent = text.value;
+  document.getElementById("items").appendChild(li);
+  text.value = " ";
+}
