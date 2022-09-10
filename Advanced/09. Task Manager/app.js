7b81a3fcb5638c01e075bf652ff10a5a047a4e31
Georgi Fotev <georgifotev12@gmail.com>
2022-09-10 22:49:09 +0300

    Adding tasks

---
+++ Advanced/09. Task Manager/app.js
@@ -0,0 +1,61 @@
+function solve() {
+  const input = {
+    name: document.getElementById("task"),
+    description: document.getElementById("description"),
+    date: document.getElementById("date"),
+  };
+
+  document.getElementById("add").addEventListener("click", addTask);
+
+  const [_, openSection, inProgressSection, completeSection] = Array.from(
+    document.querySelectorAll("section")
+  ).map((i) => i.children[1]);
+
+  function addTask(event) {
+    event.preventDefault();
+
+    const article = document.createElement("article");
+    article.appendChild(createElement("h3", input.name.value));
+    article.appendChild(
+      createElement("p", `Description: ${input.description.value}`)
+    );
+    article.appendChild(createElement("p", `Due Date: ${input.date.value}`));
+    const div = createElement("div", "", "flex");
+    const startBtn = createElement("button", "Start", "green");
+    const deleteBtn = createElement("button", "Delete", "red");
+    const finishBtn = createElement("button", "Finish", "orange");
+    div.appendChild(startBtn);
+    div.appendChild(deleteBtn);
+    article.appendChild(div);
+    openSection.appendChild(article);
+    Object.values(input).forEach((i) => (i.value = ""));
+
+    startBtn.addEventListener("click", onStart);
+    deleteBtn.addEventListener("click", onDelete);
+    finishBtn.addEventListener("click", onFinish);
+
+    function onDelete() {
+      article.remove();
+    }
+
+    function onStart() {
+      startBtn.remove();
+      div.appendChild(finishBtn);
+      inProgressSection.appendChild(article);
+    }
+
+    function onFinish() {
+      div.remove();
+      completeSection.appendChild(article);
+    }
+  }
+
+  function createElement(type, content, className) {
+    const element = document.createElement(type);
+    element.textContent = content;
+    if (className) {
+      element.className = className;
+    }
+    return element;
+  }
+}
