3ef47fdb02987d2701c9eed0dc0591b789e1c92c
Georgi Fotev <georgifotev12@gmail.com>
2022-10-03 07:52:20 +0300

    Created app scafold

---
+++ JS_Web/JS_Backend/Workshop/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const hbs = require("express-handlebars").create({
+  extname: ".hbs",
+});
+
+const app = express();
+
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/static", express.static("static"));
+
+app.listen(3000);
