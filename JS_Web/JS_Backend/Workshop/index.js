491c7f51f7461278304564525f835bbfdadfef6d
Georgi Fotev <georgifotev12@gmail.com>
2022-10-04 09:50:39 +0300

    creating the views

--- JS_Web/JS_Backend/Workshop/index.js
+++ JS_Web/JS_Backend/Workshop/index.js
@@ -3,11 +3,25 @@ const hbs = require("express-handlebars").create({
   extname: ".hbs",
 });
 
+const homeController = require("./controllers/homeCOntroller");
+const catalogController = require("./controllers/catalogController");
+const createController = require("./controllers/createController");
+const defaultController = require("./controllers/defaultController");
+const defaultTitle = require("./middlewares/defaultTitle");
+
 const app = express();
 
 app.engine(".hbs", hbs.engine);
 app.set("view engine", ".hbs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static("static"));
+
+app.use(defaultTitle("SoftUni Accomodation"));
+
+app.use(homeController);
+app.use("/catalog", catalogController);
+app.use("/create", createController);
+//in the end
+app.all("*", defaultController);
 
 app.listen(3000);
