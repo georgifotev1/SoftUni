afd369a671fb1ddd0ed89e9090b3f8fe28528e83
georgifotev1 <georgifotev12@gmail.com>
2022-10-18 17:40:05 +0300

    updating task

--- JS_Web/JS_Backend/Exam-prep/courses/config/database.js
+++ JS_Web/JS_Backend/Exam-prep/courses/config/database.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-//TODO change database according to assignment !!
-const CONNECTION_STRING = "mongodb://localhost:27017/scaffoldDb";
+const CONNECTION_STRING = "mongodb://localhost:27017/tutorials";
 module.exports = async (app) => {
   try {
     await mongoose.connect(CONNECTION_STRING, {
