2de2ce7b4d41a3caa9fc08c85efe7e0f00788839
georgifotev1 <georgifotev12@gmail.com>
2022-10-28 13:41:12 +0300

    Initializing new task

---
+++ JS_Web/JS_Backend/Exam-prep/crypto-trade/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+//TODO change database according to assignment !!
+const CONNECTION_STRING = "mongodb://localhost:27017/crypto-trade";
+module.exports = async (app) => {
+  try {
+    await mongoose.connect(CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
