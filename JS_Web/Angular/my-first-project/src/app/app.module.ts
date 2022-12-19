32e9fb6dc29833a0af0278788290377bdd22e009
georgifotev1 <georgifotev12@gmail.com>
2022-12-19 21:01:00 +0200

    Angular certificate

--- JS_Web/Angular/my-first-project/src/app/app.module.ts
+++ JS_Web/Angular/my-first-project/src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+
+const myProvider: Provider = {
+  useValue:123,
+  provide: "test"
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +17,7 @@ import { ListComponent } from './list/list.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [myProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
